e859e7ec1333f9fe6f9fac1ba6e328c7576eda47
jesse-prpic <13812@coderacademy.edu.au>
2024-06-26 15:10:57 +1000

    created the beginning menu of the terminal application

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import sys
+from termcolor import colored
 
 def print_menu():
     print("Let's Play Wordle")
@@ -12,3 +14,17 @@ def random_word():
 print_menu()
 word = random_word()
 print(word)
+
+for tries in range (1, 8):
+    guess = input().lower()
+
+    for i in range(min(len(guess), 6) and range(max(len(guess)), 6)):
+        if guess[i] == word[i]:
+            print(colored(guess[i], 'green'), end="")
+        elif guess [i] in word:
+            print(colored(guess[i], 'blue'), end="")
+        else:
+            print(guess[i], end ="")
+        
+        if guess == word:
+            print(colored(f"Congratulations! You got the world in {i}", 'pink'))
